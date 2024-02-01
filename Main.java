6e90bf21ab6220f55e3eff59870e959f6925c294
nigri <nigrickii@gmail.com>
2024-02-01 16:58:51 +0300

    добавили кол-во попыток

--- Main.java
+++ Main.java
@@ -4,14 +4,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Угадайте число от 1 до 5:");
+        System.out.println("Введите количество попыток:");
+        int trials = scanner.nextInt();
+        System.out.println("Угадайте число от 1 до 100:");
         Random random = new Random();
-        int randomInt = random.nextInt(1,6);
+        int randomInt = random.nextInt(1,101);
         int num = scanner.nextInt();
+
+        while (num != randomInt) {
+            trials = trials - 1;
+            if (trials == 0) {
+                System.out.println("Неправильно. Превышено количество попыток (" + trials + ")");
+                break;
+            }
+            System.out.println("Неправильно, попробуйте еще раз:");
+            num = scanner.nextInt();
+        }
+
         if (num == randomInt) {
-            System.out.println("ok");
-        } else {
-            System.out.println("no");
+            System.out.println("Правильно!");
         }
     }
 }
